65a64d5ccf8328aeef40506a9f30e94449c95421
Sam Waller <samuellwaller@gmail.com>
2017-11-03 01:28:05 -0400

    basic bootstrap implementation

--- index.js
+++ index.js
@@ -5,39 +5,44 @@ const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
+//DUMMY DATA FOR NOW
+const cafes = [
+    {
+        name: "Beans N Cream",
+        image: "https://farm3.staticflickr.com/2002/2431818545_bc3de6e0fd.jpg"
+    },
+    {
+        name: "Lava Java",
+        image: "https://farm3.staticflickr.com/2742/4327593145_f273d3fd53.jpg"
+    },
+    {
+        name: "Calhoun Lounge",
+        image: "https://farm3.staticflickr.com/2826/9641299964_e4af33a207.jpg"
+    },
+    {
+        name: "The Den",
+        image: "https://farm4.staticflickr.com/3455/3237137264_84e618d70a.jpg"
+    }
+]
+
 app.get("/", function(req, res){
     res.render("landing")
 })
 
 app.get("/cafes", function(req, res){
-    //DUMMY DATA FOR NOW
-    const cafes = [
-        {
-            name: "Beans N Cream",
-            image: "https://farm3.staticflickr.com/2002/2431818545_bc3de6e0fd.jpg"
-        },
-        {
-            name: "Lava Java",
-            image: "https://farm3.staticflickr.com/2742/4327593145_f273d3fd53.jpg"
-        },
-        {
-            name: "Calhoun Lounge",
-            image: "https://farm3.staticflickr.com/2826/9641299964_e4af33a207.jpg"
-        },
-        {
-            name: "The Den",
-            image: "https://farm4.staticflickr.com/3455/3237137264_84e618d70a.jpg"
-        },
-        
-    ]
+
     res.render("cafes", {cafes: cafes})
 })
 
 app.post("/cafes", function(req, res){
     const name = req.body.name
     const image = req.body.image
-
-
+    const newCafe = {
+        name: name,
+        image: image
+    }
+    cafes.push(newCafe)
+    res.redirect("/cafes")
 })
 
 app.get("/cafes/new", function(req, res){
