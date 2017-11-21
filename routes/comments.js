2564fc6e0f0b1074bf1f4305fcd2c8dc7fb5bbda
Sam Waller <samuellwaller@gmail.com>
2017-11-21 02:47:49 -0500

    comment eddit and delete

--- routes/comments.js
+++ routes/comments.js
@@ -39,6 +39,38 @@ router.post("/", isLoggedIn, function(req, res){
     })
 })
 
+// comment edit
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back")
+        } else {
+            res.render("comments/edit", {cafe_id: req.params.id, comment: foundComment})        
+        }
+    })
+
+})
+
+// comment update
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back")
+        } else {
+            res.redirect("/cafes/" + req.params.id)
+        }
+    })
+})
+
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back")
+        } else {
+            res.redirect("/cafes/" + req.params.id)
+        }
+    })
+})
 
 // Middleware
 function isLoggedIn(req, res, next){
